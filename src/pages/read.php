46a5e10e7f95a06c8db1c141e706bc2734a3eb1b
HermanoJr11 <estudantejuniorunipe11@gmail.com>
2023-11-28 21:33:56 -0300

    tentei estilizar

--- src/pages/read.php
+++ src/pages/read.php
@@ -18,55 +18,61 @@ if($mysqli->connect_errno) {
     // Inclua o código para exibir a tabela de usuários aqui
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="icon" href="./components/phantomtrupe.webp">
+    <link rel="stylesheet" href="../../css/read.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Usuários</title> 
 </head>
 <body>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Email</th>
-                <th scope="col">Telefone</th>
-                <th scope="col">Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            // Busca usuários do banco de dados usando a conexão estabelecida
-            $users = readUserAction($mysqli);
+    <div class="fundo">
 
-            // Exibe os usuários na tabela
-            if ($users && count($users) > 0) {
-                foreach ($users as $index => $user) {
-            ?>
-            <tr>
-                <th scope="row"><?= ($index + 1) ?></th>
-                <td><?= htmlspecialchars($user['name']) ?></td>
-                <td><?= htmlspecialchars($user['email']) ?></td>
-                <td><?= htmlspecialchars($user['phone']) ?></td>
-                <td>
-                    <a href="./edit.php?id=<?= $user['id'] ?>">Editar</a>
-                    <a href="./delete.php?id=<?= $user['id'] ?>">Remover</a>
-                </td>
-            </tr>
-            <?php
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Telefone</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                // Busca usuários do banco de dados usando a conexão estabelecida
+                $users = readUserAction($mysqli);
+
+                // Exibe os usuários na tabela
+                if ($users && count($users) > 0) {
+                    foreach ($users as $index => $user) {
+                        ?>
+                <tr>
+                    <th scope="row"><?= ($index + 1) ?></th>
+                    <td><?= htmlspecialchars($user['name']) ?></td>
+                    <td><?= htmlspecialchars($user['email']) ?></td>
+                    <td><?= htmlspecialchars($user['phone']) ?></td>
+                    <td>
+                        <a href="./edit.php?id=<?= $user['id'] ?>">Editar</a>
+                        <a href="./delete.php?id=<?= $user['id'] ?>">Remover</a>
+                    </td>
+                </tr>
+                <?php
+                    }
+                } else {
+                    // Caso nenhum usuário seja encontrado
+                    ?>
+                <tr>
+                    <td colspan="5">Nenhum usuário encontrado.</td>
+                </tr>
+                <?php
                 }
-            } else {
-                // Caso nenhum usuário seja encontrado
-            ?>
-            <tr>
-                <td colspan="5">Nenhum usuário encontrado.</td>
-            </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
 <?php
